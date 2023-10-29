4fb728f3b193b6c952d15b5b3b6e8580dab9d58c
Tanya Zakharova <sdfrthb23@yandex.ru>
2023-10-29 20:34:28 +0500

    fix case

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import styles from "./app.module.css";
+import styles from "./App.module.css";
 import React from "react";
 import AppHeader from "../AppHeader/AppHeader";
 import BurgerIngredients from "../BurgerIngredients/BurgerIngredients";
